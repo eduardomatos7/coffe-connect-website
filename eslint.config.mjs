5328cd42c51c2d9e14d94f7760f9fdc86a60ea7d
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-08-18 20:39:11 -0300

    chore: initial setup in next with tailwind, eslint and routes by default

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["**/node_modules/**"],
+    rules: {
+      "linebreak-style": ["error", "unix"],
+    },
+  },
+];
+
+export default eslintConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="grid place-content-center h-screen">
+      <h1>Welcome to the Coffee Connect Website</h1>
+      <p>Your one-stop solution for all coffee-related needs!</p>
+    </div>
+  );
+}
