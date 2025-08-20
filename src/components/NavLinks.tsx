4fcb2c3eb3932684eb4bfb7d6c52e838319a78d6
Weldon <weldonbarros179@gmail.com>
2025-08-20 08:52:59 -0300

    feat(nav-links): centralizar e padronizar links de navegação

---
+++ src/components/NavLinks.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+interface NavLink {
+  href: string;
+  label: string;
+}
+
+interface NavLinksProps {
+  links: NavLink[];
+  onClick?: () => void;
+  isMobile?: boolean;
+}
+
+const NavLinks: React.FC<NavLinksProps> = ({ links, onClick, isMobile }) => (
+  <ul
+    className={
+      isMobile
+        ? "flex flex-col space-y-4 font-medium text-[#8b5e3c]"
+        : "hidden space-x-8 font-medium text-[#8b5e3c] lg:flex"
+    }
+  >
+    {links.map((link) => (
+      <li key={link.href}>
+        <a
+          href={link.href}
+          onClick={onClick}
+          className="transition-colors duration-200 hover:text-[#4e2c1e] focus:outline-none"
+        >
+          {link.label}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
+
+export default NavLinks;
