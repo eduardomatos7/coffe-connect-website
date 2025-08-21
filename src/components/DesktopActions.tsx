ddfc7db04c7077dfd3ec63d322c3e22e1c6e03f1
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-08-20 22:03:12 -0300

    feat: remove footer, centraliza links do menu e muda icone e adiciona plugin eslint para remover imports nao utilizados

--- src/components/DesktopActions.tsx
+++ src/components/DesktopActions.tsx
@@ -1,22 +1,22 @@
 "use client";
 
-import { FiShoppingCart } from "react-icons/fi";
+import { IoBagOutline } from "react-icons/io5";
 
 const DesktopActions = () => (
-  <div className="hidden items-center gap-3 lg:flex">
+  <div className="hidden w-full items-center justify-end gap-3 lg:flex">
     <a
       href="#signup"
-      className="border border-[#8b5e3c] px-6 py-2 text-[#8b5e3c] transition-colors duration-200 hover:bg-[#e8d6c0] hover:text-[#27140b] focus:outline-none"
+      className="rounded-lg border border-[#8b5e3c] px-4 py-1.5 text-[#8b5e3c] transition-colors duration-400 hover:bg-[#e8d6c0] hover:text-[#734726] focus:outline-none"
     >
       Cadastrar
     </a>
     <a
       href="#login"
-      className="bg-[#8b5e3c] px-6 py-2 text-white transition-colors duration-200 hover:bg-[#70442a] focus:outline-none"
+      className="mr-3 rounded-lg bg-[#8b5e3c] px-4 py-1.5 text-white transition-colors duration-400 hover:bg-[#70442a] focus:outline-none"
     >
       Entrar
     </a>
-    <FiShoppingCart className="cursor-pointer text-3xl text-gray-800" />
+    <IoBagOutline className="cursor-pointer text-3xl text-[#8b5e3c] transition-colors duration-400 hover:text-[#a48974]" />
   </div>
 );
 
