2e96514059567ea91b153c2ff30ed36d30f09b37
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-08-21 13:14:33 -0300

    feat: cria logica para autenticar usuario

---
+++ src/app/api/auth/signin/route.ts
@@ -0,0 +1,30 @@
+import { signIn } from "@/schemas/authForm";
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../../../../../prisma/database";
+import { verifyPassword } from "@/lib/hashPassword";
+import { signJwt } from "@/lib/jwt";
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const { email, password } = body;
+
+  if (typeof email !== "string" || typeof password !== "string")
+    return NextResponse.json({ message: "Todos os campos são obrigatórios" }, { status: 400 });
+
+  const parse = signIn.safeParse(body);
+  if (!parse.success) return NextResponse.json({ error: "Dados inválidos" }, { status: 400 });
+
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) {
+    return NextResponse.json({ message: "Usuário não encontrado!" }, { status: 404 });
+  }
+  const isValidPassword = await verifyPassword(password, user.password);
+  if (!isValidPassword) {
+    return NextResponse.json({ message: "Senha incorreta!" }, { status: 401 });
+    // para uma segurança maior, seria melhor informar apenas "usuario ou senha incorretos" para os dois casos, mas aqui nao tem nada nao
+  }
+  const payload = { sub: user.id.toString(), email: user.email, name: user.name! };
+  const token = signJwt(payload);
+
+  return NextResponse.json({ token }, { status: 200 });
+}
