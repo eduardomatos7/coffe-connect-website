01f8cacf6f5de7573ee2336f3e56c65d89ceff35
Weldon <weldonbarros179@gmail.com>
2025-08-21 16:17:11 -0300

    feat: atualizar conteúdo da página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import HeroSection from "@/app/(site)/HomeSection/page";
+
 export default function Home() {
   return (
-    <div className="grid h-screen place-content-center">
-      <h1>Welcome to the Coffee Connect Website</h1>
-      <p>Your one-stop solution for all coffee-related needs!</p>
-    </div>
+    <main>
+      <HeroSection />
+    </main>
   );
 }
