9385318e42157e6c2757d694489f4904fc17f9b9
Weldon <weldonbarros179@gmail.com>
2025-08-28 13:07:37 -0300

    Refatora src/app/page.tsx para integração com seção de especialistas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/app/(site)/HomeSection/page";
-import AboutSection from "@/app/(site)/AboutSection/page";
+import AboutSection from "@/app/(site)/AboutSection/page"
+import SpecialistsSection from "./(site)/SpecialistsSection/page";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
-      <AboutSection />
+      <AboutSection/>
+      <SpecialistsSection/>
     </main>
   );
 }
