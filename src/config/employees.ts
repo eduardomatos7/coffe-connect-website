10a90856386a71bca304849bcea1f5d416570cf4
Weldon <weldonbarros179@gmail.com>
2025-08-28 13:08:15 -0300

    feat(config): adiciona arquivo employees.ts com dados dos especialistas

---
+++ src/config/employees.ts
@@ -0,0 +1,30 @@
+import employee1 from "@/public/assets/images/employee01.jpg";
+import employee2 from "@/public/assets/images/employee02.jpg";
+import employee3 from "@/public/assets/images/employee03.jpg";
+
+export const employees = [
+  {
+    name: "João Silva",
+    role: "Barista",
+    reason:
+      "Apaixonado por café e atendimento, se juntou à Coffee Connect para compartilhar experiências únicas com cada cliente.",
+    highlight: "Mestre do Espresso",
+    image: employee1,
+  },
+  {
+    name: "Maria Oliveira",
+    role: "Gerente de Operações",
+    reason:
+      "Buscando crescimento profissional e um ambiente inovador, Maria trouxe sua experiência para otimizar nossos processos.",
+    highlight: "Líder Inovadora",
+    image: employee2,
+  },
+  {
+    name: "Carlos Souza",
+    role: "Especialista em Torra",
+    reason:
+      "Com experiência em torrefação, Carlos entrou na Coffee Connect para garantir a qualidade excepcional de cada grão.",
+    highlight: "Guru da Torra",
+    image: employee3,
+  },
+];
