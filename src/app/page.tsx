1d580d01a0c2542138c382cf466236f6a418dfa1
Weldon <weldonbarros179@gmail.com>
2025-09-05 08:42:26 -0300

    style: atualizar globals.css e page.tsx para estilização da ContactSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/app/(site)/HomeSection/page";
 import AboutSection from "@/app/(site)/AboutSection/page";
+import ContanctSection from "./(site)/ContactSection/page";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <AboutSection />
+      <ContanctSection />
     </main>
   );
 }
